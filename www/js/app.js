fc630296f015ba58fe59eae049de9f7355edb30b
phixme <hickstricks@gmail.com>
2016-08-29 16:33:07 -0700

    formatting

--- www/js/app.js
+++ www/js/app.js
@@ -7,124 +7,123 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'ionic.cloud', 'starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform, $ionicDeploy, $ionicPopup, $interval) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+  .run(function ($ionicPlatform, $ionicDeploy, $ionicPopup, $interval) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
 
 
-    //polling for an update every second
-    var snapshotInterval = $interval(function() {
-      $ionicDeploy.check().then(function (snapshotAvailable) {
-        console.log('checking for update ', snapshotAvailable);
-        if (snapshotAvailable) {
-          $interval.cancel(snapshotInterval);
-          console.log('downloading update');
-          $ionicDeploy.download().then(function () {
-            console.log('extracting udate');
-            $ionicDeploy.extract().then(function () {
-              console.log('loading update');
-              // $ionicDeploy.load();
+      //polling for an update every second
+      var snapshotInterval = $interval(function () {
+        $ionicDeploy.check().then(function (snapshotAvailable) {
+          console.log('checking for update ', snapshotAvailable);
+          if (snapshotAvailable) {
+            $interval.cancel(snapshotInterval);
+            console.log('downloading update');
+            $ionicDeploy.download().then(function () {
+              console.log('extracting udate');
+              $ionicDeploy.extract().then(function () {
+                console.log('loading update');
+                // $ionicDeploy.load();
 
-              $ionicPopup.show({
-                title: 'Update available',
-                subTitle: 'A new update is available, would you like to update to the latest version?  ',
-                buttons: [
-                  { text: 'Not now' },
-                  {
-                    text: 'Restart',
-                    onTap: function(e) {
-                      $ionicDeploy.load();
+                $ionicPopup.show({
+                  title: 'Update available',
+                  subTitle: 'A new update is available, would you like to update to the latest version?  ',
+                  buttons: [
+                    {text: 'Not now'},
+                    {
+                      text: 'Restart',
+                      onTap: function (e) {
+                        $ionicDeploy.load();
+                      }
                     }
-                  }
-                ]
-              });
+                  ]
+                });
 
+              });
             });
-          });
+          }
+        });
+      }, 1000);
+
+
+    });
+  })
+
+  .config(function ($stateProvider, $urlRouterProvider, $ionicCloudProvider) {
+
+
+    $ionicCloudProvider.init({
+      "core": {
+        "app_id": "603fae71"
+      }
+    });
+
+
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
+
+    // setup an abstract state for the tabs directive
+      .state('tab', {
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'templates/tabs.html'
+      })
+
+      // Each tab has its own nav history stack:
+
+      .state('tab.dash', {
+        url: '/dash',
+        views: {
+          'tab-dash': {
+            templateUrl: 'templates/tab-dash.html',
+            controller: 'DashCtrl'
+          }
+        }
+      })
+
+      .state('tab.chats', {
+        url: '/chats',
+        views: {
+          'tab-chats': {
+            templateUrl: 'templates/tab-chats.html',
+            controller: 'ChatsCtrl'
+          }
+        }
+      })
+      .state('tab.chat-detail', {
+        url: '/chats/:chatId',
+        views: {
+          'tab-chats': {
+            templateUrl: 'templates/chat-detail.html',
+            controller: 'ChatDetailCtrl'
+          }
+        }
+      })
+
+      .state('tab.account', {
+        url: '/account',
+        views: {
+          'tab-account': {
+            templateUrl: 'templates/tab-account.html',
+            controller: 'AccountCtrl'
+          }
         }
       });
-    }, 1000);
 
-
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/dash');
 
   });
-})
-
-.config(function($stateProvider, $urlRouterProvider, $ionicCloudProvider) {
-
-
-  $ionicCloudProvider.init({
-    "core": {
-      "app_id": "603fae71"
-    }
-  });
-
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
-
-});
